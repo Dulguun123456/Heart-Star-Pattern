75cf317723f33f94f0ce8290476ecb3f22bdcf15
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-06-07 19:52:35 +0800

    Add files via upload

---
+++ HeartStarPatternWithName.cpp
@@ -0,0 +1,73 @@
+/*
+Heart Star Pattern
+/number 10/
+col  
+row1 --*****-----*****--
+row2 -*******---*******-
+row3 *********-*********
+row1 *******************
+row2 -*****************-
+row3 --***************--
+row4 ---*************---
+row5 ----***********----
+row6 -----*********-----
+row7 ------*******------
+row8 -------*****-------
+row9 --------***--------
+row10---------*---------
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int num;
+	cout<<"Enter the size(10 please):";
+	cin>>num;
+	for(int row=1;row<=num-7;row++)
+	{
+		for(int space=1;space<=num/2-row-2;space++)
+		{
+			cout<<"-";
+		}
+		for(int star=1;star<=((num/2)-2+(2*row));star++)
+		{
+			cout<<"*";
+		}
+		for(int space1=1;space1<=((num/2)+2-(row*2));space1++)
+		{
+			cout<<"-";
+		}
+		for(int star1=1;star1<=((num/2)-2+(2*row));star1++)
+		{
+			cout<<"*";
+		}	
+		cout<<endl;
+	} 
+	for(int row=1;row<=1;row++)
+	{
+		for(int star=1;star<=(num/2)-3;star++)
+		{
+		cout<<"*";
+		}
+		cout<<"AndyCodeAcademy**";
+		
+		cout<<endl;
+	}
+
+	for(int row1=1;row1<=num;row1++)
+	{
+		for(int space2=1;space2<=num-10+row1-1;space2++)
+		{
+			cout<<"-";
+		}
+		for(int star2=1;star2<=(2*(num-row1)+1);star2++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
+
